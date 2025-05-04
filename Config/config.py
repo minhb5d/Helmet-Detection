bdd70b3a618ccb7c971ade66d97cd81ef9137b9d
Nguyễn Minh <meann2424@gmail.com>
2025-05-05 02:27:06 +0700

    config model

--- Config/config.py
+++ Config/config.py
@@ -1,1 +1,25 @@
-
+model_params = {
+    'backbone_out_channels': 512,
+    'scales': [128, 256, 512],
+    'aspect_ratios': [0.5, 1.0, 2.0],
+    'rpn_bg_threshold': 0.3,
+    'rpn_fg_threshold': 0.7,
+    'rpn_nms_threshold': 0.7,
+    'rpn_batch_size': 256,
+    'rpn_pos_fraction': 0.5,
+    'rpn_train_topk': 2000,
+    'rpn_test_topk': 1000,
+    'rpn_train_prenms_topk': 12000,
+    'rpn_test_prenms_topk': 6000,
+    'roi_batch_size': 128,
+    'roi_pos_fraction': 0.25,
+    'roi_iou_threshold': 0.5,
+    'roi_low_bg_iou': 0.0,
+    'roi_nms_threshold': 0.5,
+    'roi_topk_detections': 100,
+    'roi_score_threshold': 0.05,
+    'roi_pool_size': 7,
+    'fc_inner_dim': 1024,
+    'min_im_size': 600,
+    'max_im_size': 1000
+}
